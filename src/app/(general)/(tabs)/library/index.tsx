a22e49abdc96c448c93499becad014efd0ea4885
Andre Beolchi <andregarbini2001@gmail.com>
2025-07-16 22:36:23 -0300

    feat(wip): implement base screen for list decks

--- src/app/(general)/(tabs)/library/index.tsx
+++ src/app/(general)/(tabs)/library/index.tsx
@@ -1,29 +1,44 @@
 import { Link } from 'expo-router';
-import { LucidePlus } from 'lucide-react-native';
+import { LucideGlasses, LucidePlus } from 'lucide-react-native';
 import React from 'react';
 import { Pressable, View } from 'react-native';
+import { Button } from '~/components/ui/button';
 import { Text } from '~/components/ui/text';
 import { withClassName } from '~/lib/with-classname';
+import { useGetDecks } from '~/modules/deck';
 
 withClassName(LucidePlus)
 
 export default function LibraryScreen() {
-  const deck = {
-    id: 'deck123',
-    title: 'Sample Deck',
-    description: 'A sample deck of cards for demonstration purposes.',
-  }
+  const { data: decks } = useGetDecks({ status: 'public' })
 
   return (
-    <View className='flex-1 p-6'>
-      <View>
-        <Text className='text-2xl font-semibold text-card-foreground'>
-          {deck.title}
-        </Text>
-        <Text className='text-sm text-muted-foreground'>
-          {deck.description}
-        </Text>
-      </View>
+    <View className='flex-1 gap-6 p-6'>
+      {decks?.map((deck) => (
+        <View key={deck.id} className='gap-3'>
+          <View >
+            <Text className='text-2xl font-semibold text-card-foreground'>
+              {deck.title}
+            </Text>
+            <Text className='text-sm text-muted-foreground'>
+              {deck.description}
+            </Text>
+          </View>
+
+          <Link
+            href={`/decks/${deck.id}`}
+            asChild
+          >
+            <Button
+              variant='outline'
+              className='flex-row items-center gap-3'
+            >
+              <LucideGlasses size={16} />
+              <Text className='text-card-foreground'>Estudar</Text>
+            </Button>
+          </Link>
+        </View>
+      ))}
 
       <Link
         href="/decks/create"
--- src/modules/deck.ts
+++ src/modules/deck.ts
@@ -1,5 +1,7 @@
-import { useMutation } from "@tanstack/react-query";
-import { createDeck } from "~/interfaces/sdk/deck";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { createDeck, getDecks } from "~/interfaces/sdk/deck";
+
+const QUERY_KEY = "decks";
 
 export interface ICreateDeckOptions {
   onSuccess?: (data: any) => void;
@@ -15,3 +17,15 @@ export const useCreateDeck = ({ onSuccess, onError }: ICreateDeckOptions = {}) =
     onError && onError(error);
   },
 })
+
+export interface IGetDecksOptions {
+  status?: "public" | "private";
+  creatorId?: string;
+}
+
+export const useGetDecks = ({ status, creatorId }: IGetDecksOptions = {}) => {
+  return useQuery({
+    queryKey: [QUERY_KEY, { status, creatorId }],
+    queryFn: () => getDecks({ status, creatorId }),
+  })
+}
